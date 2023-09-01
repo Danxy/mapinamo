8ddfa19dbaa4bf8cec1d0c83bab07d48b1b553ed
Daniel Broyde <nicesdan@gmail.com>
2023-09-01 13:43:23 -0400

    proj init

---
+++ Mapinamo/MapinamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapinamoApp.swift
+//  Mapinamo
+//
+//  Created by Daniel on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct MapinamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
